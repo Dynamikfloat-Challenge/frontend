d98124d21121f710241da5757c54283c67da9fed
Daniela Silva <danif.gsilva2000@gmail.com>
2025-07-18 22:20:08 +0100

    Feat: created dashbord components -> Navbar & Footer

---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="flex items-center justify-center p-4 bg-gray-100 text-gray-800 shadow-md w-full">
+      <p className="text-sm">© 2025 Daniela Silva. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
